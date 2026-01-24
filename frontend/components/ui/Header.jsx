77abd9a7374070c9d62b9087991c7b06ace96ce6
Jagoda Połchowska <j.polchowska.252@studms.ug.edu.pl>
2026-01-24 16:05:53 +0100

    Mała zmiana

--- frontend/components/ui/Header.jsx
+++ frontend/components/ui/Header.jsx
@@ -1,10 +1,17 @@
 import styles from "@/styles/Header.module.css";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.left}>
         <i className="fa-solid fa-paw" />
+        {/* <Image
+          src="/logo.svg"
+          width={35}
+          height={35}
+          alt="Logo"
+        /> */}
         <span className={styles.title}>Animal Help App</span>
       </div>
 
